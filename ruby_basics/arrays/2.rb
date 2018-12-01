acf669ad24d2ba173aec4f1743be413fa6599138
Nancy Trinh <nancytrinh20@gmail.com>
2018-12-01 05:39:58 -0800

    Add Arrays exercises.

---
+++ ruby_basics/arrays/2.rb
@@ -0,0 +1,3 @@
+pets = ['cat', 'dog', 'fish', 'lizard']
+my_pet = pets[2,3]
+puts "I have a pet #{my_pet[0]} and a pet #{my_pet[1]}!"
---
+++ ruby_basics/arrays/5.rb
@@ -0,0 +1,2 @@
+colors = ['red', 'yellow', 'purple', 'green']
+colors.each {|x| puts "I'm the color #{x}!"}
