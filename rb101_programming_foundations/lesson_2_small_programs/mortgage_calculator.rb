0370f2af3cc3c7b291ccae88d03dba0d113d6389
Nancy Trinh <nancytrinh20@gmail.com>
2018-12-04 20:06:43 -0800

    Rename variables and method.

--- rb101_programming_foundations/lesson_2_small_programs/mortgage_calculator.rb
+++ rb101_programming_foundations/lesson_2_small_programs/mortgage_calculator.rb
@@ -1,6 +1,6 @@
 require 'pry'
 
-def calculate_mortgage(input)
+def calculate_monthly_payment_monthly_payment(input)
   amount = input[:amount]
   apr = input[:apr] / 100.0
   duration_years = input[:duration_years]
@@ -11,21 +11,21 @@ def calculate_mortgage(input)
 end
 
 def test_calculator
-  # Tests the calculate_mortgage function.
+  # Tests the calculate_monthly_payment function.
   ex1 = { amount: 100, apr: 5, duration_years: 1, answer: 8.56 }
   ex2 = { amount: 1007.86, apr: 2.5, duration_years: 5, answer: 17.89 }
   ex3 = { amount: 500, apr: 1, duration_years: 2, answer: 21.05 }
 
-  puts calculate_mortgage(ex1).round(2) == ex1[:answer]
-  puts calculate_mortgage(ex2).round(2) == ex2[:answer]
-  puts calculate_mortgage(ex3).round(2) == ex3[:answer]
+  puts calculate_monthly_payment(ex1).round(2) == ex1[:answer]
+  puts calculate_monthly_payment(ex2).round(2) == ex2[:answer]
+  puts calculate_monthly_payment(ex3).round(2) == ex3[:answer]
 end
 
 def valid?(number)
   number.to_f > 0
 end
 
-details = {}
+mortgage = {}
 
 puts '=> Welcome to the Mortgage Calculator!'
 loop do
@@ -33,7 +33,7 @@ loop do
     puts '=> Please enter the loan amount. (Has to be positive number)'
     amount = gets.chomp
     if valid?(amount)
-      details[:amount] = amount.to_f
+      mortgage[:amount] = amount.to_f
       break
     else
       puts '=> Please make sure your loan amount is valid.'
@@ -44,7 +44,7 @@ loop do
     puts '=> Please enter the APR. (Has to be positive number)'
     apr = gets.chomp
     if valid?(apr)
-      details[:apr] = apr.to_f
+      mortgage[:apr] = apr.to_f
       break
     else
       puts '=> Please make sure your APR is valid.'
@@ -55,14 +55,14 @@ loop do
     puts '=> Please enter the duration in years. (Has to be positive number)'
     duration_years = gets.chomp
     if valid?(duration_years)
-      details[:duration_years] = duration_years.to_f
+      mortgage[:duration_years] = duration_years.to_f
       break
     else
       puts '=> Please make sure your duration (in years) is valid.'
     end
   end
 
-  puts "=> Your monthly payment will be #{calculate_mortgage(details)}."
+  puts "=> Your monthly payment will be #{calculate_monthly_payment(mortgage)}."
 
   puts '=> Another calculation? Enter Y to do another calculation.'
   continue = gets.chomp.upcase
