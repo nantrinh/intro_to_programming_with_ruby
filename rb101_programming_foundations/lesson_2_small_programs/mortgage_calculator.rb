d49834caac404b649842e2076753ab32d7abcfe9
Nancy Trinh <nancytrinh20@gmail.com>
2018-12-04 19:45:45 -0800

    Finish mortgage calculator.

--- rb101_programming_foundations/lesson_2_small_programs/mortgage_calculator.rb
+++ rb101_programming_foundations/lesson_2_small_programs/mortgage_calculator.rb
@@ -1,4 +1,4 @@
-require "pry"
+require 'pry'
 
 def calculate_mortgage(input)
   amount = input[:amount]
@@ -6,14 +6,65 @@ def calculate_mortgage(input)
   duration_years = input[:duration_years]
   duration_months = duration_years * 12
   monthly_interest_rate = apr / 12.0
-  monthly_payment = amount * (monthly_interest_rate / (1 - (1 + monthly_interest_rate) ** (-duration_months)))
-  # puts "Your monthly payment will be #{monthly_payment}."
+  amount * (monthly_interest_rate / (1 - \
+    (1 + monthly_interest_rate)**-duration_months))
 end
 
-ex1 = {amount: 100, apr: 5, duration_years: 1, answer: 8.56}
-ex2 = {amount: 1007.86, apr: 2.5, duration_years: 5, answer: 1007.86}
-ex3 = {amount: 500, apr: 1, duration_years: 2, answer: 21.05}
+def test_calculator
+  # Tests the calculate_mortgage function.
+  ex1 = { amount: 100, apr: 5, duration_years: 1, answer: 8.56 }
+  ex2 = { amount: 1007.86, apr: 2.5, duration_years: 5, answer: 17.89 }
+  ex3 = { amount: 500, apr: 1, duration_years: 2, answer: 21.05 }
 
-puts calculate_mortgage(ex1).round(2) == ex1[:answer] 
-puts calculate_mortgage(ex2).round(2) == ex2[:answer]
-puts calculate_mortgage(ex3).round(2) == ex3[:answer]
+  puts calculate_mortgage(ex1).round(2) == ex1[:answer]
+  puts calculate_mortgage(ex2).round(2) == ex2[:answer]
+  puts calculate_mortgage(ex3).round(2) == ex3[:answer]
+end
+
+def valid?(number)
+  number.to_f > 0
+end
+
+details = {}
+
+puts '=> Welcome to the Mortgage Calculator!'
+loop do
+  loop do
+    puts '=> Please enter the loan amount. (Has to be positive number)'
+    input = gets.chomp
+    if valid?(input)
+      details[:amount] = input.to_f
+      break
+    else
+      puts '=> Please make sure your loan amount is valid.'
+    end
+  end
+
+  loop do
+    puts '=> Please enter the APR. (Has to be positive number)'
+    input = gets.chomp
+    if valid?(input)
+      details[:apr] = input.to_f
+      break
+    else
+      puts '=> Please make sure your APR is valid.'
+    end
+  end
+
+  loop do
+    puts '=> Please enter the duration in years. (Has to be positive number)'
+    input = gets.chomp
+    if valid?(input)
+      details[:duration_years] = input.to_f
+      break
+    else
+      puts '=> Please make sure your duration (in years) is valid.'
+    end
+  end
+
+  puts "Your monthly payment will be #{calculate_mortgage(details)}."
+
+  puts 'Another calculation? Enter Y to do another calculation.'
+  input = gets.chomp.upcase
+  break if input != 'Y'
+end
