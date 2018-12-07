809e671cdb72f4161f0acef9be5f611d06db99d8
Nancy Trinh <nancytrinh20@gmail.com>
2018-12-07 14:23:22 -0800

    Add lizard and spock option.

---
+++ rb101_programming_foundations/lesson_2_small_programs/rock_paper_scissors/rpsls.rb
@@ -0,0 +1,53 @@
+require 'pry'
+
+VALID_CHOICES = %w(rock paper scissors lizard spock).freeze
+
+WINNING_COMBINATIONS = {'rock': ['scissors', 'lizard'],
+                        'paper': ['rock', 'spock'],
+                        'scissors': ['paper', 'lizard'],
+                        'spock': ['scissors', 'rock'],
+                        'lizard': ['paper', 'spock']}
+
+def prompt(message)
+  puts "=> #{message}"
+end
+
+def win?(first, second)
+  WINNING_COMBINATIONS.each_key do |k|
+    WINNING_COMBINATIONS[k].each do |v|
+      (first == k) && (second == v)
+    end
+  end
+end
+
+def display_result(player, computer)
+  if win?(player, computer)
+    prompt 'You won!'
+  elsif win?(computer, player)
+    prompt 'Computer won!'
+  else
+    prompt "It's a tie!"
+  end
+end
+
+choice = ''
+
+loop do
+  loop do
+    prompt "Choose one: #{VALID_CHOICES.join(', ')}"
+    choice = gets.chomp
+    break if VALID_CHOICES.include?(choice)
+    prompt 'That is not a valid choice.'
+  end
+
+  computer_choice = VALID_CHOICES.sample
+
+  prompt "You chose #{choice}; Computer chose: #{computer_choice}"
+
+  display_result(choice, computer_choice)
+  prompt 'Do you want to play again?'
+  answer = gets.chomp
+  break unless answer.downcase.start_with?('y')
+end
+
+prompt 'Thank you for playing. Goodbye!'
