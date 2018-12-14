bced30ad0e498ab85cb7761ea6370f84a7cc4657
Nancy Trinh <nancytrinh20@gmail.com>
2018-12-13 18:28:45 -0800

    Implement player turn

--- rb101/lesson_6/tictactoe.rb
+++ rb101/lesson_6/tictactoe.rb
@@ -11,27 +11,52 @@
 # 9. If yes, go to #1
 # 10. Good bye!
 
-def display_board(brd)
+INITIAL_MARKER = ' '
+PLAYER_MARKER = 'X'
+COMPUTER_MARKER = 'O'
+
+def prompt(message)
+  puts "=> #{message}"
+end
+
+def display_board(board)
   puts ""
   puts "     |     |"
-  puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
+  puts "  #{board[1]}  |  #{board[2]}  |  #{board[3]}"
   puts "     |     |"
   puts "-----+-----+-----"
   puts "     |     |"
-  puts "  #{brd[4]}  |  #{brd[5]}  |  #{brd[6]}"
+  puts "  #{board[4]}  |  #{board[5]}  |  #{board[6]}"
   puts "     |     |"
   puts "-----+-----+-----"
   puts "     |     |"
-  puts "  #{brd[7]}  |  #{brd[8]}  |  #{brd[9]}"
+  puts "  #{board[7]}  |  #{board[8]}  |  #{board[9]}"
   puts "     |     |"
   puts ""
 end
 
 def initialize_board
   new_board = {}
-  (1..9).each{ |num| new_board[num] = ' ' }
+  (1..9).each{ |num| new_board[num] = INITIAL_MARKER }
   new_board
 end
 
+def empty_squares(board)
+  board.keys.select { |x| board[x] == INITIAL_MARKER }
+end
+
+def player_places_piece!(board)
+  square = ''
+  loop do
+    prompt "Choose a square (#{empty_squares(board).join(', ')}):"
+    square = gets.chomp.to_i
+    break if empty_squares(board).include?(square)
+    prompt "That is not a valid choice."
+  end
+    board[square] = PLAYER_MARKER 
+end
+
 board = initialize_board
+display_board(board)
+player_places_piece!(board)
 display_board(board)
