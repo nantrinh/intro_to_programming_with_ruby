ed1c1026173daa0207071b1316f87c9419cccdae
Nancy Trinh <nancytrinh20@gmail.com>
2018-12-15 21:30:19 -0800

    Add joinor function

--- rb101/lesson_6/tictactoe.rb
+++ rb101/lesson_6/tictactoe.rb
@@ -10,37 +10,45 @@
 # 8. Play again?
 # 9. If yes, go to #1
 # 10. Good bye!
+require 'pry'
 
-INITIAL_MARKER = ' '
-PLAYER_MARKER = 'X'
-COMPUTER_MARKER = 'O'
+INITIAL_MARKER = ' '.freeze
+PLAYER_MARKER = 'X'.freeze
+COMPUTER_MARKER = 'O'.freeze
+WINNING_COMBOS = [[1, 2, 3], [4, 5, 6], [7, 8, 9],
+                  [1, 4, 7], [2, 5, 8], [3, 6, 9],
+                  [1, 5, 9], [3, 5, 7]].freeze
 
 def prompt(message)
   puts "=> #{message}"
 end
 
+# rubocop:disable Metrics/MethodLength
+# rubocop:disable Metrics/AbcSize
 def display_board(board)
   system('clear') || system('cls')
   puts "You are a #{PLAYER_MARKER}. " \
        "Computer is #{COMPUTER_MARKER}"
-  puts ""
-  puts "     |     |"
+  puts ''
+  puts '     |     |'
   puts "  #{board[1]}  |  #{board[2]}  |  #{board[3]}"
-  puts "     |     |"
-  puts "-----+-----+-----"
-  puts "     |     |"
+  puts '     |     |'
+  puts '-----+-----+-----'
+  puts '     |     |'
   puts "  #{board[4]}  |  #{board[5]}  |  #{board[6]}"
-  puts "     |     |"
-  puts "-----+-----+-----"
-  puts "     |     |"
+  puts '     |     |'
+  puts '-----+-----+-----'
+  puts '     |     |'
   puts "  #{board[7]}  |  #{board[8]}  |  #{board[9]}"
-  puts "     |     |"
-  puts ""
+  puts '     |     |'
+  puts ''
 end
+# rubocop:enable Metrics/MethodLength
+# rubocop:enable Metrics/AbcSize
 
 def initialize_board
   new_board = {}
-  (1..9).each{ |num| new_board[num] = INITIAL_MARKER }
+  (1..9).each { |num| new_board[num] = INITIAL_MARKER }
   new_board
 end
 
@@ -54,9 +62,9 @@ def player_places_piece!(board)
     prompt "Choose a square (#{empty_squares(board).join(', ')}):"
     square = gets.chomp.to_i
     break if empty_squares(board).include?(square)
-    prompt "That is not a valid choice."
+    prompt 'That is not a valid choice.'
   end
-    board[square] = PLAYER_MARKER 
+  board[square] = PLAYER_MARKER
 end
 
 def computer_places_piece!(board)
@@ -69,55 +77,55 @@ def board_full?(board)
 end
 
 def someone_won?(board)
-  !!detect_winner(board)
+  true if detect_winner(board)
 end
 
 def detect_winner(board)
-  winning_combos = [[1, 2, 3], [4, 5, 6], [7, 8, 9],
-                   [1, 4, 7], [2,5,8], [3,6,9],
-                   [1,5,9],[3,5,7]]
-  winning_combos.each do |combo|
-    if board[combo[0]] == PLAYER_MARKER &&
-       board[combo[1]] == PLAYER_MARKER &&
-       board[combo[2]] == PLAYER_MARKER
-      return 'Player'
-    elsif board[combo[0]] == COMPUTER_MARKER &&
-          board[combo[1]] == COMPUTER_MARKER &&
-          board[combo[2]] == COMPUTER_MARKER
-      return 'Computer'
-    end
+  WINNING_COMBOS.each do |combo|
+    values = board.values_at(*combo)
+    return 'Player' if values.all? { |x| x == PLAYER_MARKER }
+    return 'Computer' if values.all? { |x| x == COMPUTER_MARKER }
   end
   nil
+end
+
+def joinor(arr, sep = ', ', word = 'or')
+  if arr.size == 2
+    puts "#{arr[0]} #{word} #{arr[1]}"
+  else
+    puts arr[0...-1].map(&:to_s).join(sep) \
+                    .concat("#{sep}#{word} #{arr[-1]}")
+  end
 end
 
 loop do
   board = initialize_board
   loop do
     display_board(board)
-  
+
     player_places_piece!(board)
     break if someone_won?(board) || board_full?(board)
-  
+
     computer_places_piece!(board)
     break if someone_won?(board) || board_full?(board)
   end
-  
+
   display_board(board)
-  
+
   if someone_won?(board)
     prompt "#{detect_winner(board)} won!"
   else
     prompt "It's a tie!"
   end
 
-  prompt "Play again? (y/n)"
+  prompt 'Play again? (y/n)'
   answer = ''
-  loop do 
+  loop do
     answer = gets.chomp.downcase
-    break if %w(y n).include?(answer)
-    prompt "Please enter y or n."
+    break if %w[y n].include?(answer)
+    prompt 'Please enter y or n.'
   end
-  break if answer == "n"
+  break if answer == 'n'
 end
 
-prompt "Thanks for playing!"
+prompt 'Thanks for playing!'
