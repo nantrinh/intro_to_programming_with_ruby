b806b6220d59f974ae5a04016c5ca4acbbf28a39
Nancy Trinh <nancytrinh20@gmail.com>
2019-02-22 19:59:41 -0800

    Update octal

--- rb130/challenges/octal.rb
+++ rb130/challenges/octal.rb
@@ -1,46 +1,22 @@
-
-# A number in octal only contains the digits 0..7
-
 class Octal
-  attr_accessor :chars
+  BASE = 8
   def initialize(str)
-    @chars = str.chars 
-    remove_leading_zeros(chars)
-  end
-
-  def remove_leading_zeros(chars)
-    while chars.first == '0'
-      chars.shift
-    end
+    @str = str 
   end
 
   def to_decimal
-    return 0 unless number? and octal?
-    # split number into its digits
-    # reverse the array of digits
-    # iterate through each digit
-    # compute digit * (base ** index) 
-    # have an accumulator to conduct the sum
-    acc = 0
-    chars.reverse.map(&:to_i).each_with_index do |x, idx|
-      acc += x * (8 ** idx)
+    valid? ? calculate : 0
+  end
+
+  def valid?
+    @str =~ /\A[0-7]+\z/
+  end
+
+  def calculate
+    res = 0 
+    @str.chars.reverse.map(&:to_i).each_with_index do |x, idx|
+      res += x * (BASE ** idx)
     end
-    acc
-  end
-
-  def number?
-    chars.join.to_i.to_s == chars.join 
-  end
-
-  def octal?
-    chars.map(&:to_i).all? {|x| (0..7).include?(x)}
+    res
   end
 end
-
-# p (Octal.new('1').to_decimal        == 1)
-# p (Octal.new('10').to_decimal       == 8)
-# p (Octal.new('17').to_decimal       == 15) 
-# p (Octal.new('11').to_decimal       == 9)
-# p (Octal.new('8').to_decimal        == 0)
-# p (Octal.new('carrot').to_decimal   == 0)
-# p (Octal.new('6789').to_decimal     == 0)
