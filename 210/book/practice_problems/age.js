d72bbb9432177764d0e7260d328af521c44334c9
Nancy Trinh <nancytrinh20@gmail.com>
2019-04-13 20:54:00 -0700

    Finish arrays chapter

--- 210/book/practice_problems/age.js
+++ 210/book/practice_problems/age.js
@@ -1,4 +1,8 @@
 let rlSync = require('readline-sync');
 let age = Number(rlSync.question('How old are you? '));
 console.log(`You are ${age} years old.`);
-console.log(`In 10 years, you will be ${age + 10} years old.`);
+for (let i=10; i<=40; i+=10) {
+  console.log(`In ${i} years, you will be ${age + i} years old.`);
+}
+
+
--- 210/book/practice_problems/greeter.js
+++ 210/book/practice_problems/greeter.js
@@ -1,4 +1,9 @@
-let rlSync = require('readline-sync');
-let firstName = rlSync.question('What is your first name? ');
-let lastName = rlSync.question('What is your last name? ');
-console.log(`Hello, ${firstName} ${lastName}!`);
+function fullName() {
+  let rlSync = require('readline-sync');
+  let firstName = rlSync.question('What is your first name? ');
+  let lastName = rlSync.question('What is your last name? ');
+  return `${firstName} ${lastName}`
+}
+
+
+console.log(`Hello, ${fullName()}!`);
