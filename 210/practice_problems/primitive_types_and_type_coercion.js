a7ccbee331a721b4bb386025dfc068c5ac64a171
Nancy Trinh <nancytrinh20@gmail.com>
2019-04-18 13:42:34 -0700

    Add lecture notes and practice problems

---
+++ 210/practice_problems/primitive_types_and_type_coercion.js
@@ -0,0 +1,14 @@
+var x = '13';
+var y = 9;
+
+// console.log(Number(x) + y);
+console.log(x * y); // 117, -,*,/, and % are only defined for numbers
+
+var npa = 212;
+var nxx = 555;
+var num = 1212;
+
+console.log(String(npa) + String(nxx) + String(num));
+
+console.log(true.toString()); // 'true'
+console.log([1, 2, 3].toString()); // '1,2,3'
---
+++ 210/practice_problems/variables_and_numbers.js
@@ -0,0 +1,18 @@
+let numerator = 10;
+let denominator = 2;
+let answer = numerator / denominator;
+console.log(answer);  // 5
+
+let incrementer = 1;
+let start = incrementer;
+let end;
+let difference;
+incrementer += 3;
+incrementer += 2; // style guide says do not use ++
+end = incrementer;
+difference = end - start;
+console.log(end); // 6
+console.log(difference); // 5
+
+answer = (11 + 31) * 3;
+console.log(answer); // 126
