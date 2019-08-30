6757b49667b35db82540a78fde942f64344bdf01
Nancy Trinh <nancytrinh20@gmail.com>
2019-08-30 13:58:14 -0700

    WIP

--- 230/exercises/guess-a-word/javascripts/game.js
+++ 230/exercises/guess-a-word/javascripts/game.js
@@ -1,24 +1,36 @@
+function indexesOfMatches(str, letter) {
+  var indexes = [];
+  var i;
+  for (i=0; i < str.length ; i++) {
+    if (str[i] === letter) {
+      indexes.push(i); 
+    }
+  } 
+  return indexes;
+};
+
+var randomWord = (function() {
+  var words = ["apple", "banana", "orange", "pear"];
+
+  return function() {
+    var word = words[Math.floor(Math.random() * words.length)]; 
+    words.splice(words.indexOf(word), 1);
+    return word;
+  };
+})();
+
 $(function () {
-  var randomWord = (function() {
-    var words = ["apple", "banana", "orange", "pear"];
-
-    return function() {
-      var word = words[Math.floor(Math.random() * words.length)]; 
-      words.splice(words.indexOf(word), 1);
-      return word;
-    };
-  })();
-
   function Game() {
     this.word = randomWord(); 
     this.incorrect = 0;
+    this.mistakes_left = 6;
     this.letters_guessed = new Set();
     this.correct_spaces = 0;
     if (!this.word) {
       this.displayMessage("Sorry, I've run out of words!"); 
       return;
     }
-    this.word = this.word.split("");
+    //this.word = this.word.split("");
     this.init();
   };
 
@@ -31,9 +43,11 @@ $(function () {
     },
     displayMessage: function(text) {
       $message.text(text);
+      $message.removeClass("invisible");
     },
     init: function() {
       this.createBlanks(); 
+      $play_again.addClass("invisible");
     }
   }
 
@@ -41,14 +55,34 @@ $(function () {
   var $letters= $("#spaces");
   var $guesses = $("#guesses");
   var $apples = $("#apples");
+  var $play_again = $("#replay");
 
   var game = new Game();
 
   $(document).keypress(function(e) {
     if (e.keyCode >= 97 && e.keyCode <= 122) {
-
-      game.letters_guessed.add(String.fromCharCode(e.keyCode));
-      console.log(game.letters_guessed);
+      $message.addClass("invisible");
+      var letter = String.fromCharCode(e.keyCode);
+      if (game.letters_guessed.has(letter)) {
+        game.displayMessage(`You have already guessed "${letter.toUpperCase()}".`);
+      } else {
+        game.letters_guessed.add(letter);
+        var indexes = indexesOfMatches(game.word, letter);
+        if (indexes.length > 0) {
+          indexes.forEach(function(index) {
+            game.$spaces.eq(index).text(letter);
+          });
+        } else {
+          game.displayMessage(`"${letter.toUpperCase()}" is not in the word.`);
+          game.incorrect += 1;
+          $apples.addClass(`guess_${game.incorrect}`);
+          game.mistakes_left -= 1;
+          if (game.mistakes_left === 0) {
+            game.displayMessage("Sorry, you've run out of guesses!"); 
+            $play_again.removeClass("invisible");
+          }
+        }
+      }
     }
   });
 });
